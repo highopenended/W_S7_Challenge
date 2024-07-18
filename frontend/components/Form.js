7320075962934db4229b28343b3cc9c403f00c99
Matt Glenn <highopenended@gmail.com>
2024-07-18 13:42:39 -0400

    Finally managed to implement some front-end validation

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,34 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import * as Yup from "yup";
 
-
-
-// 👇 Here are the validation errors you will use with Yup.
-const validationErrors = {
-  fullNameTooShort: 'full name must be at least 3 characters',
-  fullNameTooLong: 'full name must be at most 20 characters',
-  sizeIncorrect: 'size must be S or M or L'
-}
-
-// 👇 Here you will create your schema.
-// const schema = Yup.object().shape({
-//   // 'name' is expected to be a string, mandatory, and accompanied by a warning message if absent
-//   name: Yup.string()
-//     .required("fullName is required")
-//     .min(3,validationErrors.fullNameTooShort)
-//     .max(20,validationErrors.fullNameTooLong),
-
-//   // Similarly, 'email' is also expected to be a string conforming to an email pattern, and it's mandatory
-//   size: Yup.string()
-//     .required("size is required")
-//     .oneOf(['S','M','L'],validationErrors.sizeIncorrect),
-
-//   // 'age' is validated against a minimum value and includes a corresponding warning message
-//   age: Yup.number()
-//     .min(18, "Must be at least 18 years old")
-//     .required("Age is required"),
-// });
-
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
   { topping_id: '1', text: 'Pepperoni' },
@@ -38,7 +10,77 @@ const toppings = [
   { topping_id: '5', text: 'Ham' },
 ]
 
+
+
+
+
+
+// 👇 Here are the validation errors you will use with Yup.
+const validationErrors = {
+  fullNameTooShort: 'full name must be at least 3 characters',
+  fullNameTooLong: 'full name must be at most 20 characters',
+  sizeIncorrect: 'size must be S or M or L'
+}
+
+
+// 👇 Here you will create your schema.
+const schema = Yup.object().shape({
+  fullName: Yup
+    .string()
+    .required(validationErrors.fullNameTooShort)
+    .min(3,validationErrors.fullNameTooShort)
+    .max(20,validationErrors.fullNameTooLong),
+
+  size: Yup
+    .string()
+    .required()
+    .oneOf(['S','M','L'],validationErrors.sizeIncorrect),
+});
+ 
+
+
+
+
+
+const initialValues={fullName:'',size:''}
+const initialErrors={fullName:'',size:''}
+
+
 export default function Form() {
+  const [values, setValues] = useState(initialValues)
+  const [errors, setErrors] = useState(initialErrors)
+  const [submitEnabled, setSubmitEnabled] = useState(false)
+
+
+  useEffect(() => {
+    schema.isValid(values).then((isValid) => {
+      setSubmitEnabled(isValid)
+    });
+  }, [values]);
+
+  const changeHandler=(evt)=>{
+    let name
+    let val
+    if(evt.target.type==="checkbox"){
+      name=evt.target.name
+      val=evt.target.checked
+    }else{
+      name=evt.target.id
+      val=evt.target.value
+    }
+    setValues({...values, [name]:val})
+    
+    Yup
+      .reach(schema,name)
+      .validate(val)
+      .then(()=>{
+        setErrors({...errors,[name]:''})
+      })
+      .catch((err)=>{
+        setErrors({...errors,[name]:err.errors[0]})
+      })
+  }
+
   return (
     <form>
       <h2>Order Your Pizza</h2>
@@ -48,28 +90,30 @@ export default function Form() {
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input onChange={changeHandler} placeholder="Type full name" id="fullName" name="fullName" type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.fullName && <div className='error'>{errors.fullName}</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
-            <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
+          <select onChange={changeHandler} id="size" name="size">
+            <option value>----Choose Size----</option>
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.size && <div className='error'>{errors.size}</div>}
       </div>
 
       <div className="input-group">
-        {/* 👇 Maybe you could generate the checkboxes dynamically */}
         {toppings.map(topping=>{
           return <label key={topping.topping_id}>
             <input
-              name={topping.text}
+              onChange={changeHandler}
+              name={topping.topping_id}
               type="checkbox"
             />
             {topping.text}<br />
@@ -78,7 +122,9 @@ export default function Form() {
         
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input disabled={!submitEnabled} type="submit" />
     </form>
+    
   )
+  
 }
